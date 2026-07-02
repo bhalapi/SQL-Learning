f3f1225816c640527d4bbd6204ae500608a05362
bhalapi <bhalapi74@gmail.com>
2026-07-02 18:09:34 -0400

    -- Lessons Learned: -- -- SELECT chooses columns. -- FROM chooses the table. -- WHERE filters rows. -- ORDER BY sorts the results.

--- SQL/Case002_Organizing_Evidence.sql
+++ SQL/Case002_Organizing_Evidence.sql
@@ -1,15 +1,27 @@
--- ========================================
+-- ==========================================
 -- Case 002 - Organizing Evidence
 --
 -- Scenario:
 -- The lieutenant wants to review crash
--- reports in chronological order
+-- reports in chronological order.
 --
 -- Concepts Learned:
--- ORDER BY
--- ASC
--- DESC
--- ========================================
+-- - ORDER BY
+-- - ASC
+-- - DESC
+-- ==========================================
+
+-- Exercise 1
+SELECT *
+FROM crashes
+ORDER BY date;
+
+-- Exercise 2
+SELECT *
+FROM crashes
+ORDER BY date DESC;
+
+-- Challenge
 SELECT location, date
 FROM crashes
 WHERE severity = 'Property Damage'
