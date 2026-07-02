8bf704e753eac0efa71025bd1488a6f006487e3d
bhalapi <bhalapi74@gmail.com>
2026-07-02 17:55:47 -0400

    Restore Case 001 SQL query

---
+++ SQL/Case001_Retrieve_Crash_Records.sql
@@ -0,0 +1,13 @@
+-- ==========================================
+-- Case 001 - Retrieve Crash Records
+--
+-- Scenario:
+-- Display all crash reports from the database.
+--
+-- Concepts Learned:
+-- - SELECT
+-- - FROM
+-- ==========================================
+
+SELECT *
+FROM crashes;
